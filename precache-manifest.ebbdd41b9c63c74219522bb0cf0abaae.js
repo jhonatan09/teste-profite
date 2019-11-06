3e38fcb773da35fc46c79038aaaabdf31a0d5cfa
Jhonatan <jhonatancardoso09@gmail.com>
2019-11-06 10:06:23 -0300

    Updates

---
+++ precache-manifest.ebbdd41b9c63c74219522bb0cf0abaae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76777ebfe1124a1debfa2e18dc7c9803",
+    "url": "/teste-profite/index.html"
+  },
+  {
+    "revision": "d35f1fd1114810ff6bf0",
+    "url": "/teste-profite/static/css/main.627383bd.chunk.css"
+  },
+  {
+    "revision": "01911acbdb8f086ae6f2",
+    "url": "/teste-profite/static/js/2.f48c3936.chunk.js"
+  },
+  {
+    "revision": "d35f1fd1114810ff6bf0",
+    "url": "/teste-profite/static/js/main.ef7b8373.chunk.js"
+  },
+  {
+    "revision": "8fd1002ef402e0e8c6ee",
+    "url": "/teste-profite/static/js/runtime-main.c242aca5.js"
+  },
+  {
+    "revision": "4f85cfd5e5b9633adc413f42de019d04",
+    "url": "/teste-profite/static/media/Layer4.4f85cfd5.png"
+  },
+  {
+    "revision": "8bde822bc6490781b1cd672ef5aa3d99",
+    "url": "/teste-profite/static/media/ipanema-beach-99388_1280.8bde822b.jpg"
+  },
+  {
+    "revision": "55cabf7326e57c2b9365b9db57e13a38",
+    "url": "/teste-profite/static/media/jaguar-2894706_1280.55cabf73.jpg"
+  },
+  {
+    "revision": "acfc4fef97a01f5abef290aba729045a",
+    "url": "/teste-profite/static/media/johnatan.acfc4fef.jpg"
+  }
+]);
